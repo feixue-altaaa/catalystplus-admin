27a7a53f90e5482b3cf32edf0bdbe936274ef6aa
fangsheng <fs9990123@163.com>
2022-11-17 10:47:12 +0800

    接口返回形式修改

--- src/main/java/com/catalystplus/admin/controller/api/UserApi.java
+++ src/main/java/com/catalystplus/admin/controller/api/UserApi.java
@@ -2,10 +2,11 @@ package com.catalystplus.admin.controller.api;
 
 
 import com.catalystplus.admin.response.Response;
-import com.catalystplus.admin.response.user.UserDataResponse;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @Api(value = "UserApi", tags = "用户数据统计接口")
 @RequestMapping("/admin")
@@ -13,22 +14,22 @@ public interface UserApi {
 
     @GetMapping("/auth/user/tun")
     @ApiOperation("查询所有的用户数")
-    Response<UserDataResponse> getTNU();
+    Response<Long> getTNU();
 
     @PostMapping("/auth/user/nnut")
     @ApiOperation("查询今日新增的用户数")
-    Response<UserDataResponse> getNNUT(String dateKey);
+    Response<Long> getNNUT(String dateKey);
 
     @PostMapping("/auth/user/au")
     @ApiOperation("查询日周月用户活跃数，日周月活占比")
-    Response<UserDataResponse> getDWMAU(String dateKey);
+    Response<Map<String, Object>> getDWMAU(String dateKey);
 
     @GetMapping("/auth/user/nacu")
     @ApiOperation("查询同时在线活跃用户数")
-    Response<UserDataResponse> getNACU();
+    Response<Long> getNACU();
 
     @GetMapping("/auth/user/nncu")
     @ApiOperation("查询同时在线新用户数")
-    Response<UserDataResponse> getNNCU();
+    Response<Long> getNNCU();
 
 }
